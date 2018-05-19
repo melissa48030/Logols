39daafaf140c209fdb6c3190f139e617c88ce211
melissa48030 <39421080+melissa48030@users.noreply.github.com>
2018-05-19 15:55:41 -0400

    Assign2

--- Day1/csharp/Assign1/Program.cs
+++ Day1/csharp/Assign1/Program.cs
@@ -12,42 +12,46 @@ namespace Assign1
             Given an int variable, write if else statements and console out the persons status.
             Using the same int variable, modify your code to perform the same operation with a switch statement.
              */
-
-            int value = 4;
             
-            if(value == 1)
+            int[]statuses  = new int[] {2,1,4,3};
+            for(int count = 0; count < statuses.Length; count++)
             {
-                Console.WriteLine("IF Status: Alive");
-            }
-            else if (value == 2)
-            {
-                Console.WriteLine("IF Status: Zombie");
-            }
-            else if (value == 3)
-            {
-                Console.WriteLine ("IF Status: Dead");
-            }
+                int value = statuses[count];
+                if(value == 1)
+                    {
+                      Console.WriteLine("IFAR Status: Alive");
+                     }
+                else if (value == 2)
+                    {
+                      Console.WriteLine("IFAR Status: Zombie");
+                    }
+                 else if (value == 3)
+                     {
+                      Console.WriteLine ("IFAR Status: Dead");
+                      }
             else if (value == 4)
-            {
-                Console.WriteLine("IF Status: Unknown");
-            } 
-
+                        {
+                Console.WriteLine("IFAR Status: Unknown");
+                        } 
+            
+        
             //switch statement
             switch (value)
             {
                 case 1:
-                    Console.WriteLine("SWITCH Status: Alive");
+                    Console.WriteLine("SWITCHAR Status: Alive");
                     break;
                 case 2:
-                    Console.WriteLine("SWITCH Status: Zombie");
+                    Console.WriteLine("SWITCHAR Status: Zombie");
                     break;
                 case 3:
-                    Console.WriteLine ("SWITCH Status: Dead");
+                    Console.WriteLine ("SWITCHAR Status: Dead");
                     break;
                 case 4:
-                    Console.WriteLine("SWITCH Status: Unknown");
+                    Console.WriteLine("SWITCHAR Status: Unknown");
                     break;
-            } 
-        }
+            }
+            }
+        }   
     }
 }
